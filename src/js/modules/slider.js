2a32058485f061f98fae02cc29449f8d81d7b988
yuriipetrenko <obzora6@gmail.com>
2021-03-27 17:27:08 +0200

    add main big slider

---
+++ src/js/modules/slider.js
@@ -0,0 +1,69 @@
+export default class Slider{
+    constructor(page, btns){
+        this.page = document.querySelector(page);
+        this.btns = document.querySelectorAll(btns);
+        this.slides = this.page.children;
+        this.slideIndex = 1;
+    }
+
+    showSlides(n){
+        if(n > this.slides.length){
+            this.slideIndex = 1;
+        }
+        
+        if(n < 1){
+            this.slideIndex = this.slides.length;
+        }
+
+        try{
+            this.hanson.style.opacity = '0';
+            
+            if(n === 3){
+                this.hanson.classList.add('animated');
+                setTimeout(()=>{
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 1000)
+            }else{
+                  this.hanson.classList.remove('slideInUp')  
+            }
+        }catch(e){
+            console.log(e);
+        }
+
+        this.slides.forEach(slide =>{
+            slide.style.display = 'none';
+        })
+
+        this.slides[this.slideIndex - 1].style.display = 'block'
+
+    }
+
+    plusSlides(n){
+        this.showSlides(this.slideIndex += n);
+    }
+
+    render(){
+        try{
+            this.hanson = document.querySelector('.hanson')
+        }catch(e){
+            console.log(e)
+        }
+
+        this.btns.forEach(item=>{
+            item.addEventListener('click', ()=>{
+                this.plusSlides(1)
+            })
+
+            item.parentNode.previousElementSibling.addEventListener('click', (e)=>{
+                e.preventDefault();
+                this.slideIndex = 1
+                this.showSlides(this.slideIndex)
+            })
+        })
+
+        this.showSlides(this.slideIndex)
+    }
+
+
+}
